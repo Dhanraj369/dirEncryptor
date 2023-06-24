2dfe3979e82c25acc7ea7c7b25989cbbb803f2b9
Mr . Dark Sage <99752941+Dhanraj369@users.noreply.github.com>
2023-06-24 16:02:28 +0530

    Update app.js
    I have added comments explaining each section of the code , hope useful

--- app.js
+++ app.js
@@ -1,88 +1,83 @@
 const fs = require('fs');
 const prompt = require('prompt-sync')();
-var childProcess = require('child_process');
-let isErr = false;
+const childProcess = require('child_process');
 
 const name = prompt('What is your code folder/encrypted file name? - ');
-if (fs.existsSync(name)) {
-    if (name.endsWith("encrypted")) {
-        const pass = prompt('What should be password? - ');
-        if (fs.existsSync(`./edtor/decrypt.js`)) {
-            fs.rmdirSync(`./edtor/decrypt.js`, { recursive: true });
-        }
-        var decrypt = fs.createWriteStream(`./edtor/decrypt.js`, {
-            flags: 'a'
-        });
-        decrypt.write(`import * as folderEncrypt from 'folder-encrypt';\n`
-            + `folderEncrypt.decrypt({\n`
-            + ` password: '${pass}',\n`
-            + ` input: '${name}',\n`
-            + ` output: '${name.replace(".encrypted", "")}'\n`
-            + `}).then(() => {\n`
-            + ` console.log('Decrypted!');\n`
-            + `}).catch((err) => {\n`
-            + ` console.log(err);\n`
-            + `});`);
 
-        runScript('./edtor/decrypt.js', function (err) {
+if (fs.existsSync(name)) { // Check if the specified file or folder exists
+    if (name.endsWith("encrypted")) { // If the name ends with "encrypted"
+        const pass = prompt('What should be the password? - ');
+
+        if (fs.existsSync('./editor/decrypt.js')) { // Check if the decrypt.js file exists
+            fs.unlinkSync('./editor/decrypt.js'); // Remove the existing decrypt.js file
+        }
+
+        const decryptCode = `import * as folderEncrypt from 'folder-encrypt';
+        folderEncrypt.decrypt({
+            password: '${pass}',
+            input: '${name}',
+            output: '${name.replace(".encrypted", "")}'
+        }).then(() => {
+            console.log('Decrypted!');
+        }).catch((err) => {
+            console.log(err);
+        });`;
+
+        fs.writeFileSync('./editor/decrypt.js', decryptCode); // Write the decryption code to decrypt.js
+
+        runScript('./editor/decrypt.js', function (err) { // Execute the decryption script
             if (err) {
                 throw err;
             }
-            //fs.rmdirSync(`./${name}`, { recursive: true });
-            //fs.rmdirSync(`./edtor/decrypt.js`, { recursive: true });
-
+            fs.rmdirSync(`./${name}`, { recursive: true }); // Remove the decrypted folder
+            fs.unlinkSync('./editor/decrypt.js'); // Remove the decrypt.js file
         });
-    }
-    else {
-        const pass = prompt('What should be password? - ');
-        if (fs.existsSync(`./edtor/encrypt.js`)) {
-            fs.rmdirSync(`./edtor/encrypt.js`, { recursive: true });
+    } else { // If the name does not end with "encrypted"
+        const pass = prompt('What should be the password? - ');
+
+        if (fs.existsSync('./editor/encrypt.js')) { // Check if the encrypt.js file exists
+            fs.unlinkSync('./editor/encrypt.js'); // Remove the existing encrypt.js file
         }
-        var encrypt = fs.createWriteStream(`./edtor/encrypt.js`, {
-            flags: 'a'
-        });
-        encrypt.write(`import * as folderEncrypt from 'folder-encrypt';\n`
-            + `folderEncrypt.encrypt({\n`
-            + ` password: '${pass}',\n`
-            + ` input: 'codes',\n`
-            + ` output: 'codes.encrypted'\n`
-            + `}).then(() => {\n`
-            + ` console.log('Encrypted!');\n`
-            + `}).catch((err) => {\n`
-            + ` console.log(err);\n`
-            + `});`);
 
-        runScript('./edtor/encrypt.js', function (err) {
-            if (err) throw err;
-            fs.rmdirSync(name, { recursive: true });
-            fs.rmdirSync(`./edtor/encrypt.js`, { recursive: true });
+        const encryptCode = `import * as folderEncrypt from 'folder-encrypt';
+        folderEncrypt.encrypt({
+            password: '${pass}',
+            input: 'codes',
+            output: 'codes.encrypted'
+        }).then(() => {
+            console.log('Encrypted!');
+        }).catch((err) => {
+            console.log(err);
+        });`;
+
+        fs.writeFileSync('./editor/encrypt.js', encryptCode); // Write the encryption code to encrypt.js
+
+        runScript('./editor/encrypt.js', function (err) { // Execute the encryption script
+            if (err) {
+                throw err;
+            }
+            fs.rmdirSync(name, { recursive: true }); // Remove the original folder or file
+            fs.unlinkSync('./editor/encrypt.js'); // Remove the encrypt.js file
         });
     }
-}
-else {
-    console.log(`${name} doesn't exists.`);
+} else {
+    console.log(`${name} doesn't exist.`); // If the specified file or folder does not exist
 }
 
 function runScript(scriptPath, callback) {
-
-    // keep track of whether callback has been invoked to prevent multiple invocations
     var invoked = false;
-
     var process = childProcess.fork(scriptPath);
 
-    // listen for errors as they may prevent the exit event from firing
     process.on('error', function (err) {
         if (invoked) return;
         invoked = true;
         callback(err);
     });
 
-    // execute the callback once the process has finished running
     process.on('exit', function (code) {
         if (invoked) return;
         invoked = true;
         var err = code === 0 ? null : new Error('exit code ' + code);
         callback(err);
     });
-
 }
